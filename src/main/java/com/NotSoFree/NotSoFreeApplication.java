1af679735f525959e4497f1eac3a37b915da3dbb
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-07-28 16:14:49 -0300

    Completed the appliaction.properties

--- src/main/java/com/NotSoFree/NotSoFreeApplication.java
+++ src/main/java/com/NotSoFree/NotSoFreeApplication.java
@@ -2,9 +2,8 @@ package com.NotSoFree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) //1#
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class }) //1#
 public class NotSoFreeApplication {
 
 	public static void main(String[] args) {
