f3591194f5f31ac3d0756a2a43d0ff915db23837
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-07-28 23:14:32 -0300

    I show the products in the index

--- src/main/java/com/NotSoFree/web/indexC.java
+++ src/main/java/com/NotSoFree/web/indexC.java
@@ -1,6 +1,8 @@
 package com.NotSoFree.web;
 
+import com.NotSoFree.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexC {
     
+    @Autowired
+    private ProductService productService;
+    
     @GetMapping("/")
     public String index(Model model){
         log.info("index handler");
         
-        model.addAttribute("Message", "Welcome to my app");
+        model.addAttribute("products", productService.listProducts());
         return "index";
     }
 }
