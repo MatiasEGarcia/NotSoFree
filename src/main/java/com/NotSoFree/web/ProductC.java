2c2fbc4d2fadacc87bebbf4d0237f75ab86212dd
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-07-30 21:39:32 -0300

    Added the save product function

--- src/main/java/com/NotSoFree/web/ProductC.java
+++ src/main/java/com/NotSoFree/web/ProductC.java
@@ -2,6 +2,7 @@ package com.NotSoFree.web;
 
 import com.NotSoFree.domain.Product;
 import com.NotSoFree.service.ProductService;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
@@ -36,5 +38,37 @@ public class ProductC {
         return "redirect:/";
     }
     
+    @GetMapping(value="savePage")
+    public String savePage(Model model){
+        log.info("savePage handler");
+        
+        Product product= new Product();
+        
+        model.addAttribute("product", product);
+        return "saveProdPage";
+    }
+    
+    
+    
+    @PostMapping(value="saveProd")
+    public String saveProduct(Product product,
+            @RequestParam(name = "file", required = false) MultipartFile image,
+            RedirectAttributes redirectAttrs){
+         log.info("saveProd handler");
+         
+        try {
+            productService.saveProduct(product, image);
+        }catch (IOException ex) {
+            log.info("There was some error with the image: {}",ex);
+        }catch(Exception ex){
+             log.info("There was some error: {}",ex);
+        }
+         
+        redirectAttrs
+            .addFlashAttribute("message", "Product create successfully")
+            .addFlashAttribute("class", "success");
+
+        return "redirect:/productC/savePage";
+    }
     
 }
