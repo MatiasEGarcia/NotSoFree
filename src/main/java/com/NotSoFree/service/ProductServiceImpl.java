7ed7a8858c336f33dcf3567cd06d6dc7ecc2250b
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-09-03 16:17:50 -0300

    page detailProduct

--- src/main/java/com/NotSoFree/service/ProductServiceImpl.java
+++ src/main/java/com/NotSoFree/service/ProductServiceImpl.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
--- src/main/java/com/NotSoFree/web/ProductC.java
+++ src/main/java/com/NotSoFree/web/ProductC.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -152,4 +153,10 @@ public class ProductC {
         return "listAllProducts";
     }
 
+    @GetMapping(value = "/detailPage/{idProduct}")
+    public String detailPage(@PathVariable String idProduct, Model model) throws Exception{
+        log.info("detailPage handler");
+        model.addAttribute("productDto", productService.findProduct(Long.parseLong(idProduct) ) );
+        return "detailProduct";
+    }
 }
